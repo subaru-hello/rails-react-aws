61b8b0b536dc0c44a42911b541b491934165fd1b
subaru-hello <octosubaru0926@gmail.com>
2023-04-30 22:10:54 +0900

    copy

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # ヘルスチェック
+  get '/health_check', to: 'health_checks#index'
+  resources :blogs, only: [:index, :show, :create]
 end
