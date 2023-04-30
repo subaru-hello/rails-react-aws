88ed3b381ba9eecdf7e0466c28cb395d7230ec21
subaru-hello <octosubaru0926@gmail.com>
2023-04-30 23:06:00 +0900

    add blog settings

--- blog-web/src/index.js
+++ blog-web/src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blog from "./routes/blog.js";
+import Blogs from "./routes/blogs.js";
+import CreateBlog from "./routes/createBlog.js";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="create" element={<CreateBlog />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="blogs/:id" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ blog-web/src/routes/blogs.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { axiosInstance } from "../utils/axios.js";
+
+function Blogs() {
+  const [blogs, setBlogs] = useState();
+  useEffect(() => {
+    const f = async () => {
+      const res = await axiosInstance.get("/blogs");
+      setBlogs(res.data);
+    };
+  f();
+ }, []);
+  return (
+    <div style={{ margin: "auto", width: "1000px" }}>
+      <h1>ブログ一覧画面</h1>
+      <div>
+        <Link to="/">TOP</Link>
+      </div>
+      <div>
+        <Link to="/create">記事作成画面</Link>
+      </div>
+       <div>
+       <ul>
+         {blogs?.map((b) => (
+           <Link to={`/blogs/${b.id}`} key={b.id}>
+             <li>{b.title}</li>
+           </Link>
+         ))}
+       </ul>
+     </div>
+    </div>
+  );
+}
+
+export default Blogs;
